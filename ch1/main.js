e18ecafbd9411e87549d7fedc5eb6feb6b30362e
vicchui <vic.chui383@gmail.com>
2024-01-20 08:21:04 +0800

    add 64 green circles

--- ch1/main.js
+++ ch1/main.js
@@ -1,10 +1,15 @@
-//console.log('main.js loaded')
+
 $(document).ready(function() {
     'use strict';
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
-    var c = Shape.Circle(200, 200, 50);
-    c.fillColor = 'green';
+    var c;
+    for(var x=25; x<400; x+=50) {
+        for(var y=25; y<400; y+=50) {
+            c = Shape.Circle(x, y, 20);
+            c.fillColor = "green";
+        }
+    }
     paper.view.draw();
-    console.log('main.js loaded');
+    //console.log('main.js loaded');
 });
