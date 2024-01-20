985cda4fb6a162bf5283adbf5e3192aec9458a6e
vicchui <vic.chui383@gmail.com>
2024-01-20 08:15:46 +0800

    paint

--- ch1/main.js
+++ ch1/main.js
@@ -1,1 +1,10 @@
-console.log('main.js loaded')
+//console.log('main.js loaded')
+$(document).ready(function() {
+    'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+    var c = Shape.Circle(200, 200, 50);
+    c.fillColor = 'green';
+    paper.view.draw();
+    console.log('main.js loaded');
+});
