990f517140bbc538125866550a8bb8ca0310c24e
vicchui <vic.chui383@gmail.com>
2024-01-20 08:27:15 +0800

    add user input mouse event

--- ch1/main.js
+++ ch1/main.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
     'use strict';
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
-    var c;
-    for(var x=25; x<400; x+=50) {
-        for(var y=25; y<400; y+=50) {
-            c = Shape.Circle(x, y, 20);
-            c.fillColor = "green";
-        }
+    var tool = new Tool();
+    tool.onMouseDown = function(event) {
+     //   var c = Shape.Circle(event.point.x, event.point.y, 20);
+        var c = Shape.Circle(event.point, 20);
+     
+        c.fillColor = 'green';
     }
     paper.view.draw();
     //console.log('main.js loaded');
